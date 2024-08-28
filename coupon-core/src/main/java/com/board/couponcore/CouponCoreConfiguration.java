7b028c5645829adb95e7627ea352950201b0abe0
hanju <rondo2860@gmail.com>
2024-08-28 14:05:29 +0900

    feat: 도메인 엔티티 작성

--- coupon-core/src/main/java/com/board/couponcore/CouponCoreConfiguration.java
+++ coupon-core/src/main/java/com/board/couponcore/CouponCoreConfiguration.java
@@ -3,7 +3,10 @@ package com.board.couponcore;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @ComponentScan
 @EnableAutoConfiguration
 public class CouponCoreConfiguration {
---
+++ coupon-core/src/main/java/com/board/couponcore/model/CouponType.java
@@ -0,0 +1,13 @@
+package com.board.couponcore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+public enum CouponType {
+    FIRST_COME_FIRST_SERVED
+}
